301942461b8f30f99ee8bdacc7acac0a2fea30c3
Yuya Minamide <dettu1003@gmail.com>
2023-04-06 16:09:11 -0700

    removed unnecessary line

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
 import { Banner, Header } from "../components/index";
 import requests from "@/utils/requests";
 import { Movie } from "../typings";
@@ -55,9 +53,6 @@ export const getServerSideProps = async () => {
 			fetch(requests.fetchDocumentaries).then((res) => res.json()),
 		]);
 
-	// console.log("----------");
-	// console.log(netflixOriginals);
-	// console.log("----------");
 	return {
 		props: {
 			netflixOriginals: netflixOriginals.results,
