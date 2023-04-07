de7e86c30ee8a564f2deabace107fcb90262a0ff
Yuya Minamide <dettu1003@gmail.com>
2023-04-06 18:27:09 -0700

    added style and function for row section

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,4 +1,5 @@
 import { Movie } from "@/typings";
+import { useRef, useState } from "react";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import { Thumbnail } from "../index";
 
@@ -10,6 +11,18 @@ interface Props {
 }
 
 export function Row({ title, movies }: Props) {
+	const rowRef = useRef<HTMLDivElement>(null);
+	const [isMoved, setIsMoved] = useState(false);
+
+	const handleClick = (direction: string) => {
+		setIsMoved(true);
+		if (rowRef.current) {
+			const { scrollLeft, clientWidth } = rowRef.current;
+			const scrollTo = direction === "left" ? scrollLeft - clientWidth : scrollLeft + clientWidth;
+			rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+		}
+	};
+
 	return (
 		<div className="h-40 space-y-0.5 md:space-y-2">
 			<h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
@@ -17,16 +30,20 @@ export function Row({ title, movies }: Props) {
 			</h2>
 			<div className="group relative md:-ml-2">
 				<IoIosArrowBack
-					className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 `}
+					className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+						!isMoved && "hidden"
+					}`}
+					onClick={() => handleClick("left")}
 				/>
 
-				<div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2 ">
+				<div ref={rowRef} className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2 ">
 					{movies.map((movie) => (
 						<Thumbnail key={movie.id} movie={movie} />
 					))}
 				</div>
 				<IoIosArrowForward
 					className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
+					onClick={() => handleClick("right")}
 				/>
 			</div>
 		</div>
