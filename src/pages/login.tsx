9889c4488fe43fff7d9434d44e39e2b180ae6bba
Yuya Minamide <dettu1003@gmail.com>
2023-04-21 16:18:27 -0700

    added error message to input

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,31 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function login() {
+interface Inputs {
+	email: string;
+	password: string;
+}
+
+function Login() {
+	const [login, setLogin] = useState(false);
+
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>();
+
+	const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+		if (login) {
+			// await signIn(email, password)
+		} else {
+			// await signUp(email, password)
+		}
+	};
+
 	return (
 		<div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
 			<Head>
@@ -19,22 +43,31 @@ function login() {
 				className="cursor-pointer object-contain absolute left-4 top-4 md:left-10 md:top-6"
 			/>
 
-			<form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+			>
 				<h1 className="text-4xl font-semibold">Sign In</h1>
 				<div className="space-y-4">
 					<label className="inline-block w-full">
-						<input type="email" placeholder="Email" className="input" />
+						<input type="email" placeholder="Email" className="input" {...register("email", { required: true })} />
+						{errors.email && <p className="p-1 text-[13px] font-light  text-orange-500">Please enter a valid email.</p>}
 					</label>
 					<label className="inline-block w-full">
-						<input type="password" placeholder="Password" className="input" />
+						<input type="password" placeholder="Password" className="input" {...register("password", { required: true })} />
+						{errors.password && (
+							<p className="p-1 text-[13px] font-light  text-orange-500">
+								Your password must contain between 4 and 60 characters.
+							</p>
+						)}
 					</label>
 				</div>
-				<button className="w-full rounded bg-[#E50914] py-3 font-semibold" type="submit">
+				<button className="w-full rounded bg-[#E50914] py-3 font-semibold" type="submit" onClick={() => setLogin(true)}>
 					Sign In
 				</button>
 				<div className="text-[gray]">
 					New to Netflix?{" "}
-					<button className="cursor-pointer text-white hover:underline" type="submit">
+					<button className="cursor-pointer text-white hover:underline" type="submit" onClick={() => setLogin(false)}>
 						Sign up now
 					</button>
 				</div>
@@ -43,4 +76,4 @@ function login() {
 	);
 }
 
-export default login;
+export default Login;
