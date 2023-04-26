8e0284cfd66bfef2f39e561f2237ed01a7cbaa03
Yuya Minamide <dettu1003@gmail.com>
2023-04-26 14:21:45 -0700

    added trailer function

--- src/components/Row/Thumbnail/Thumbnail.tsx
+++ src/components/Row/Thumbnail/Thumbnail.tsx
@@ -1,4 +1,6 @@
+import { modalState, movieState } from "@/atoms/modalAtom";
 import Image from "next/image";
+import { useRecoilState } from "recoil";
 import { Movie } from "@/typings";
 
 interface Props {
@@ -8,8 +10,17 @@ interface Props {
 }
 
 export function Thumbnail({ movie }: Props) {
+	const [showModal, setShowModal] = useRecoilState(modalState);
+	const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+
 	return (
-		<div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+		<div
+			onClick={() => {
+				setCurrentMovie(movie);
+				setShowModal(true);
+			}}
+			className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+		>
 			<Image
 				src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
 				className="rounded-sm object-cover md:rounded"
